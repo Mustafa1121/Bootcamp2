11d967d29aba02bf50aa97240755ce310e7d5294
Mustafa khodor <87423616+Mustafa1121@users.noreply.github.com>
2023-02-27 20:20:12 +0200

    added the features of urlName

--- controller/urlController.js
+++ controller/urlController.js
@@ -1,24 +1,23 @@
 const Url = require("../models/urlModel");
 // const shortid = require("shortid"); // to prevent the use of a hard codded function
-const { nanoid } = require('nanoid');
 
 exports.shortenUrl = async (req, res) => {
-  const { originalUrl } = req.body;
+  const { originalUrl, urlname } = req.body;
 
   try {
-    // Check if the URL already exists in the database
-    let url = await Url.findOne({ originalUrl });
-
+    // Check if the URL already exists in the database or the name
+    let url = await Url.findOne({
+      $or: [{ originalUrl }, { urlCode: urlname }],
+    });
 
     if (!url) {
       // Generate a unique 6-character code for the URL
       // 1
       // let urlCode = shortid.generate();
-      // 2       
-      let urlCode = nanoid(8);
 
-      console.log(req.get('host'))
+      console.log(req.get("host"));
       // Construct the short URL using the current request
+      let urlCode = urlname;
       let shortUrl = `${req.protocol}://${req.get("host")}/url/${urlCode}`;
       url = new Url({
         originalUrl,
@@ -37,7 +36,7 @@ exports.shortenUrl = async (req, res) => {
 
 exports.redirectUrl = async (req, res) => {
   const { urlCodeDb } = req.params;
-  console.log(urlCodeDb)
+  console.log(urlCodeDb);
   try {
     const url = await Url.findOne({ urlCodeDb });
 
